e9a280eed563938a955c56e6c3b4f2ae88542dc2
“MohanP7” <pratyushmohanty5@gmail.com>
2025-05-05 11:23:09 +0530

    Fixed Launching errors
    Base_Class
    ----------
    1. Changed the port no- 4724(Manually started with CMD prompt)
    
    POM.xml
    ----------
    2. selenium java version - 4.11.0
    3. Java Client - 8.6.0
    4. Test Ng Version- 7.9.0

--- src/main/java/org/example/Base_Class.java
+++ src/main/java/org/example/Base_Class.java
@@ -18,14 +18,14 @@ public class Base_Class {
     @BeforeClass
     public  void Configuration() throws MalformedURLException {
         service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\HP\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
-            .withIPAddress("127.0.0.1").usingPort(4723).withTimeout(Duration.ofSeconds(60)).build();
+            .withIPAddress("127.0.0.1").usingPort(4724).withTimeout(Duration.ofSeconds(60)).build();
         service.start();
 
     UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("Pratyush_Pixel");
         options.setApp("C:\\Users\\HP\\AndroidStudioProjects\\android-apidemos\\app\\build\\outputs\\apk\\debug\\app-debug.apk");
 
-    androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+    androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4724"), options);
     androidDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 }
 @AfterClass
